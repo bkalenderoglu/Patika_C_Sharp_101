a205a837762b7dbee8a5e2e207d34c70b896c577
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-05-30 20:43:42 +0300

    readme file and practices folder added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Patika_C_Sharp_101
+# Patika C# 101
+
+This repository contains some practices and projects created by [Patika](https://app.patika.dev/)
+
+1.  [Console Programming](https://app.patika.dev/egitimler/csharp-101-patikasi/csharp-101/3-console-programlama)
---
+++ practices/console programming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Surname: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+            
+           
+        }
+    }
+}
