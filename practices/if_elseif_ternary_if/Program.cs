3c2917e8632e1574f3d76a78023f8428d9e13640
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-05-31 13:58:43 +0300

    if-elseif-ternary-if added

---
+++ practices/if_elseif_ternary_if/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace if_elseif_ternary_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >= 6 && time<= 11)
+
+                Console.WriteLine("Good Morning");
+
+            else if(time <= 18)
+                Console.WriteLine("Good afternoon!");
+            
+            else
+                Console.WriteLine("Good Evening!");
+
+            string result = time <= 18 ? "Good Afternoon!" : "Good evening!";
+
+            result = time >=6 && time < 11 ? "Good Morning!" : time <= 18 ? "Good Afternoon!" : "Good Evening!";
+
+            Console.WriteLine(result);
+        }
+    }
+}
