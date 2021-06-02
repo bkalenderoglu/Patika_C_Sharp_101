a42dd126f076d0d93239767dd261427f157d9785
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-06-02 14:59:28 +0300

    datetime and math library added

---
+++ practices/methods_Datetime-Math/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace methods_Datetime_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+            Console.WriteLine(DateTime.Now.AddDays(2));
+            Console.WriteLine(DateTime.Now.AddHours(3));
+            Console.WriteLine(DateTime.Now.AddSeconds(30));
+            Console.WriteLine(DateTime.Now.AddMonths(5));
+            Console.WriteLine(DateTime.Now.AddYears(10));
+            Console.WriteLine(DateTime.Now.AddMilliseconds(50));
+
+            //Datetime Format
+            Console.WriteLine(DateTime.Now.ToString("dd")); // 02
+            Console.WriteLine(DateTime.Now.ToString("ddd")); // Wed
+            Console.WriteLine(DateTime.Now.ToString("dddd")); // Wednesday 
+
+            Console.WriteLine(DateTime.Now.ToString("MM")); 
+            Console.WriteLine(DateTime.Now.ToString("MMM")); 
+            Console.WriteLine(DateTime.Now.ToString("MMMM")); 
+
+            Console.WriteLine(DateTime.Now.ToString("yy"));
+            Console.WriteLine(DateTime.Now.ToString("yyyy"));
+
+            Console.WriteLine("*** Math Library ***");
+            //Math library
+            Console.WriteLine(Math.Abs(-25));
+            Console.WriteLine(Math.Sin(20));
+            Console.WriteLine(Math.Cos(20));
+            Console.WriteLine(Math.Tan(20));
+
+            Console.WriteLine(Math.Ceiling(22.3)); //23
+            Console.WriteLine(Math.Round(22.3)); //22
+            Console.WriteLine(Math.Round(22.7)); //23
+            Console.WriteLine(Math.Floor(22.7)); //22
+
+            Console.WriteLine(Math.Max(22,3));
+            Console.WriteLine(Math.Min(22,3));
+
+            Console.WriteLine(Math.Pow(2,3));
+            Console.WriteLine(Math.Sqrt(9));
+            Console.WriteLine(Math.Log(9));
+            Console.WriteLine(Math.Exp(3));
+            Console.WriteLine(Math.Log10(10));
+
+
+        }
+    }
+}
