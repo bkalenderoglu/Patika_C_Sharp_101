b5635161eb8465c122d39adbf84a97502bb4a318
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-06-02 17:32:02 +0300

    ArrayList added

---
+++ practices/collectionsArrayList/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace collectionsArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // System.Collections namespace
+            ArrayList list = new ArrayList();
+            //list.Add("Beyza");
+            list.Add(2);
+            //list.Add(true);
+            //list.Add('A');
+
+            // Console.WriteLine(list[1]);
+
+            //foreach (var item in list)
+            //    Console.WriteLine(item);
+
+            // AddRange
+            Console.WriteLine("*** Add Range ***");
+            //string[] colors = {"red","yellow", "green"};
+            List<int> numbers = new List<int>(){1,8,3,7,9,92,5};
+            //list.AddRange(colors);
+            list.AddRange(numbers);
+
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            //Sort
+            Console.WriteLine("**** Sort ****");
+            list.Sort(); // error in run time cannot compare elements
+
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            //Binary Search
+            Console.WriteLine("*** Binary Search ***");
+            Console.WriteLine(list.BinarySearch(9));
+
+            //Reverse
+            Console.WriteLine("*** Reverse ***");
+            list.Reverse();
+
+            foreach (var item in list)
+                Console.WriteLine(item);
+            
+            //Clear
+            Console.WriteLine("*** Clear ***");
+            list.Clear();
+
+            foreach (var item in list)
+                Console.WriteLine(item);
+        }
+    }
+}
