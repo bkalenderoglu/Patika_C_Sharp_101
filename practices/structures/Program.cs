1afcc3a118b164f7bb03a31124126e63a9d233c1
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-06-02 21:38:06 +0300

    struct added

---
+++ practices/structures/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Rectangle rectangle = new Rectangle(); 
+          rectangle.ShortSide =  3;
+          rectangle.LongSide =  4;
+
+          System.Console.WriteLine("Rectangle area:     {0}",rectangle.RectangleArea());
+
+          RectangleStruct rectangleStruct = new RectangleStruct(3,4);
+          System.Console.WriteLine("Rectangle area:     {0}",rectangleStruct.RectangleArea());
+
+        }
+    }
+
+    class Rectangle{
+
+        public int ShortSide;
+        public int LongSide;
+        public long RectangleArea()
+        {
+            return this.ShortSide * this.LongSide;
+        }
+    }
+
+    struct RectangleStruct
+    {
+        public int ShortSide;
+        public int LongSide;
+
+        public RectangleStruct(int shortSide, int longSide)
+        {
+            ShortSide = shortSide;
+            LongSide = longSide;
+        }
+        
+        public long RectangleArea()
+        {
+            return this.ShortSide * this.LongSide;
+        }
+    }
+}
