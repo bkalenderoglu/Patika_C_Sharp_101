81697eebd31e9833ddbd76cd709e382d58a544c4
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-06-07 19:58:56 +0300

    inheritance added

---
+++ practices/Inheritance/Canlilar.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Inheritance
+{
+    public class Canlilar
+    {
+        protected void Beslenme(){
+            Console.WriteLine("Canlılar beslenir.");
+        }
+
+        protected void Solunum(){
+            Console.WriteLine("Canlılar solunum yapar.");
+        }
+        protected void Bosaltim(){
+            Console.WriteLine("Canlılar bosaltım yapar.");
+        }
+    }
+}
